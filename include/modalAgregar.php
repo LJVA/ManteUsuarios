4627df5eac1e22e792296b77c1eed16cb7f87920
LJVA <varelalonnys2442@gmail.com>
2020-07-02 14:03:37 -0600

    V4 ManteUsuarios Editar

--- include/modalAgregar.php
+++ include/modalAgregar.php
@@ -1,35 +1,34 @@
 <div id="addEmployeeModal" class="modal fade">
-       <div class="modal-dialog">
-           <div class="modal-content">
-               <form action="mantenimiento.php" method="post">
-                   <div class="modal-header">
-                       <h4 class="modal-title">Agregar Usuario</h4>
-                       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                   </div>
-                   <div class="modal-body">
-                       <div class="form-group">
-                           <label>Nombre</label>
-                           <input name="nombre" type="text" class="form-control" required>
-                       </div>
-                       <div class="form-group">
-                           <label>Correo</label>
-                           <input name="correo" type="email" class="form-control" required>
-                       </div>
-                       <div class="form-group">
-                           <label>Dirección</label>
-                           <textarea name="direccion" class="form-control" required></textarea>
-                       </div>
-                       <div class="form-group">
-                           <label>Teléfono</label>
-                           <input name="telefono" type="text" class="form-control" required>
-                       </div>
-                   </div>
-                   <div class="modal-footer">
-                       <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
-                       <input type="submit" class="btn btn-success" value="Guardar">
-                   </div>
-               </form>
-           </div>
-       </div>
-   </div>        
-
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="mantenimiento.php" method="POST">
+                <div class="modal-header">
+                    <h4 class="modal-title">Agregar Usuario</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Nombre</label>
+                        <input type="text" class="form-control" required name="nombre">
+                    </div>
+                    <div class="form-group">
+                        <label>Correo</label>
+                        <input type="email" class="form-control" required name="correo">
+                    </div>
+                    <div class="form-group">
+                        <label>Dirección</label>
+                        <textarea class="form-control" required name="direccion"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Teléfono</label>
+                        <input type="text" class="form-control" required name="telefono">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar">
+                    <input type="submit" class="btn btn-success" name="accion" value="Guardar">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>  
--- mantenimiento.php
+++ mantenimiento.php
@@ -1,51 +1,114 @@
 <?php
+ session_start();
 
-session_start();
-
-if(isset($_POST['accion'])){
-    if($_POST['accion'] == 'guardar'){
-        guardarUsuario();
-        header('location:index.php');
-    }
-}
-
-if(isset($_GET['accion'])){
-    if($_GET['accion'] == 'EliminarSesion'){
-        eliminarSesion();
-        header('location:index.php');
-    }
-}
-
-function obtenerListaUsuarios(){
+ if(isset($_POST['accion'])){
+     switch ($_POST['accion'])
+     {
+         case 'Guardar':
+                guardarUsuario();
+                header('location:index.php');
+                break;
+         case 'Guardar Cambios':
+                guardarEditar();
+                header('location:index.php');
+                break;
+         case 'Cancelar':
+                unset($_SESSION['usuarioEditar']);
+                header('location:index.php');
+                break;
+     }  
+ }
+ 
+ if(isset($_GET['accion'])){  
+     switch ($_GET['accion'])
+     {
+         case 'Eliminar':
+                eliminarUsuario();
+                header('location:index.php');
+                break;
+         case 'EliminarSesion':    
+                eliminarSesion();
+                header('location:index.php');
+                break;
+         case 'Editar':
+                editarUsuario();
+                header('location:index.php');
+                break;
+     }  
+ }
+ 
+ function obtenerListaUsuarios(){
     if(isset($_SESSION['listaUsuarios'])){
-        $listaUsuarios = $_SESSION['listaUsuarios'];
+       $listaUsuarios = $_SESSION['listaUsuarios'];
     }else{
-        $listaUsuarios = array();
-        $_SESSION['listaUsuarios'] = $listaUsuarios;
-    }
+       $listaUsuarios = array();     
+       $_SESSION['listaUsuarios'] = $listaUsuarios;
+    }   
     return $listaUsuarios;
-}
+ }
+
+ function guardarUsuario(){
+     $nombre = $_POST['nombre'];
+     $correo = $_POST['correo'];
+     $direccion = $_POST['direccion'];
+     $telefono = $_POST['telefono'];
+     
+     $usuario = array("nombre"=>$nombre,"correo"=>$correo,"direccion"=>$direccion,"telefono"=>$telefono);
+     $lista = obtenerListaUsuarios();
+     array_push($lista,$usuario);
+     $_SESSION['listaUsuarios'] = $lista;     
+ }
+ 
+ function obtenerUsuarioEditar(){
+     if(isset($_SESSION['usuarioEditar'])){
+       $usuarioEditar = $_SESSION['usuarioEditar'];
+    }else{
+       $usuarioEditar = array();     
+       $_SESSION['usuarioEditar'] = $usuarioEditar;
+    }   
+    return $usuarioEditar;
+ }
+ 
+ function editarUsuario(){
+     $posicion = $_GET['posicion'];
+     $lista = obtenerListaUsuarios();
+     $usuarioEditar = $_SESSION['usuarioEditar'];
+     $usuarioEditar = $lista[$_GET['posicion']];
+     array_push($usuarioEditar, $_GET['posicion']);
+     $_SESSION['usuarioEditar'] = $usuarioEditar;
+ }
+ 
+ function guardarEditar(){
+     $nombre = $_POST['nombre'];
+     $correo = $_POST['correo'];
+     $direccion = $_POST['direccion'];
+     $telefono = $_POST['telefono'];
+     
+     $listaEditar = obtenerUsuarioEditar();
+     $listaTodos = obtenerListaUsuarios();
+     $listaTodos[$listaEditar[0]]['nombre'] = $nombre;
+     $listaTodos[$listaEditar[0]]['correo'] = $correo;
+     $listaTodos[$listaEditar[0]]['direccion'] = $direccion;
+     $listaTodos[$listaEditar[0]]['telefono'] = $telefono;
+     
+     $_SESSION['listaUsuarios'] = $listaTodos;
+     unset($_SESSION['usuarioEditar']);
+ }
 
 
-function guardarUsuario(){
-    $nombre = $_POST['nombre'];
-    $correo = $_POST['correo'];
-    $direccion = $_POST['direccion'];
-    $telefono = $_POST['telefono'];
-    
-    $usuario = array("nombre"=>$nombre, "correo"=>$correo, "direccion"=>$direccion, "telefono"=>$telefono);
-    $lista = obtenerListaUsuarios();
-    array_push($lista, $usuario);
-    $_SESSION['listaUsuarios'] = $lista;
-}
 
-function eliminarSesion(){
-    unset($_SESSION['listaUsuarios']);
-    session_unset();
-    session_destroy();
-}
+ function eliminarUsuario(){
+     $posicion = $_GET['posicion'];
+     $lista = obtenerListaUsuarios();
+     unset($lista[$posicion]);
+     $_SESSION['listaUsuarios'] = $lista;
+ }
 
 
+ function eliminarSesion(){
+     unset($_SESSION['listaUsuarios']);
+     session_unset();
+     session_destroy();
+ }
 
 ?>
-
